804da30c23627d0f41705a26e17997bc15086617
Kaifan Zheng <77627965+kaifanzheng@users.noreply.github.com>
2022-01-21 22:45:41 -0500

    back-end maven setup and frontend file created
    back-end maven setup and frontend file created

---
+++ Co-vivre/src/main/java/com/Covivre/Covivre/CoVivreApplication.java
@@ -0,0 +1,13 @@
+package com.Covivre.Covivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoVivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoVivreApplication.class, args);
+	}
+
+}
