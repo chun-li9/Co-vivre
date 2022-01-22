ac88371cd25e93e6913b99cb8b03a0d772fc3882
Chun160712 <chun160712@gmail.com>
2022-01-22 11:31:19 -0500

    Update WebCrawlerController.java
     Fixed Access-Control-Allow-Origin problem

--- Co-vivre/src/main/java/com/Covivre/Covivre/Controller/WebCrawlerController.java
+++ Co-vivre/src/main/java/com/Covivre/Covivre/Controller/WebCrawlerController.java
@@ -2,6 +2,7 @@ package com.Covivre.Covivre.Controller;
 
 import com.Covivre.Covivre.Service.WebCrawService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping(path = "api/v1/data")
 public class WebCrawlerController {
+	
 
     private final WebCrawService webCrawService;
 
@@ -20,11 +22,13 @@ public class WebCrawlerController {
         this.webCrawService = webCrawService;
     }
 
+    
     @GetMapping("greeting")
     public String getGreeting(){
         return("welcome to covivre!~");
     }
 
+    @CrossOrigin(origins = "http://localhost:1234")
     @GetMapping("basicData")
     public ArrayList<String> getBasicData() throws IOException {
         return webCrawService.getCases();
