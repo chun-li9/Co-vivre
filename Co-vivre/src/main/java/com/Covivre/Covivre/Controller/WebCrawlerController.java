ec0bfd9e3566af3ffaea3f27ec79da022b7ca86a
Kaifan Zheng <77627965+kaifanzheng@users.noreply.github.com>
2022-01-22 01:24:59 -0500

    service
    service

--- Co-vivre/src/main/java/com/Covivre/Covivre/Controller/WebCrawlerController.java
+++ Co-vivre/src/main/java/com/Covivre/Covivre/Controller/WebCrawlerController.java
@@ -1,5 +1,7 @@
 package com.Covivre.Covivre.Controller;
 
+import com.Covivre.Covivre.Service.WebCrawService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "api/v1/data")
 public class WebCrawlerController {
+
+    private final WebCrawService webCrawService;
+
+    @Autowired
+    public WebCrawlerController(WebCrawService webCrawService){
+        this.webCrawService = webCrawService;
+    }
 
     @GetMapping("greeting")
     public String getGreeting(){
---
+++ Co-vivre/src/main/java/com/Covivre/Covivre/Service/WebCrawService.java
@@ -0,0 +1,7 @@
+package com.Covivre.Covivre.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebCrawService {
+}
